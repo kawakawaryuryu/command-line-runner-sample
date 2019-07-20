460e2c2d4539d194471d214b98187261f144f6c7
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2019-07-21 01:07:42 +0900

    add parameter

--- src/main/java/com/kawakawaryuryu/commandlinerunnersample/JobLauncher.java
+++ src/main/java/com/kawakawaryuryu/commandlinerunnersample/JobLauncher.java
@@ -15,11 +15,15 @@ public class JobLauncher implements CommandLineRunner {
 
     private final String config;
 
+    private final Integer value;
+
     public JobLauncher(
             @Value("${application.name}") String appName,
-            @Value("${config.name}") String config) {
+            @Value("${config.name}") String config,
+            @Value("${hoge.fuga.value}") Integer value) {
         this.appName = appName;
         this.config = config;
+        this.value = value;
     }
 
     @Override
@@ -30,6 +34,7 @@ public class JobLauncher implements CommandLineRunner {
     public String hoge(String fuga) {
         log.info(appName + " starting");
         log.info(config + " starting");
+        log.info(value + " starting");
         return fuga;
     }
 }
